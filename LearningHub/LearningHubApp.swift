47d2f2c3c6c0acb8bdda0e48bae176ac2143b3a4
Shrikanta Mazumder <shrikantamazumder@gmail.com>
2021-08-21 09:47:27 +0600

    - organise project structure - added content view model - added environment object

--- LearningHub/LearningHubApp.swift
+++ LearningHub/LearningHubApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct LearningHubApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
+                .environmentObject(ContentModel())
         }
     }
 }
---
+++ LearningHub/ViewModels/ContentModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentModel.swift
+//  LearningHub
+//
+//  Created by Shrikanta Mazumder on 21/8/21.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+}
