c4ff620d2db14566e93d951e1762bde2a6a69a30
Shrikanta Mazumder <shrikantamazumder@gmail.com>
2021-08-21 09:39:35 +0600

    Initial Commit

---
+++ LearningHub/LearningHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningHubApp.swift
+//  LearningHub
+//
+//  Created by Shrikanta Mazumder on 21/8/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
